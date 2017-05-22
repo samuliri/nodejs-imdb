34c04a281236548823f85613a1ade1058c90a573
Samuli Ristimäki <samuli.ristimaki@a-lehdet.fi>
2017-05-23 01:45:52 +0300

    Basic sort and pagination logic.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,49 @@
 const routes = require('express').Router();
 
 routes.get ('/', function(req, res){
-    var filmsdata = req.app.locals.filmdata;
+    var filmsdata = req.app.locals.filmdata.films;
+
+    //Sort title
+    if (typeof req.query.title !== 'undefined') {
+      filmsdata.sort(function(a, b) {
+        return (a.Title < b.Title) ? -1 : (a.Title > b.Title) ? 1 : 0;
+      });
+    }
+
+    //Sort year
+    if (typeof req.query.year !== 'undefined') {
+      filmsdata.sort(function(a, b) {
+        return parseFloat(a.Year) - parseFloat(b.Year);
+      });
+    }
+
+    //Pagination
+    var totalFilms = filmsdata.length,
+        pageSize = 8,
+        pageCount = totalFilms/8,
+        currentPage = 1,
+        films = [],
+        filmsArrays = [],
+        filmsList = [];
+
+    for (var i = 1; i < totalFilms; i++) {
+      films.push({poster: filmsdata[i]['Poster'], year: filmsdata[i]['Year'], title: filmsdata[i]['Title']});
+    }
+
+    while (films.length > 0) {
+      filmsArrays.push(films.splice(0, pageSize));
+    }
+
+    if (typeof req.query.page !== 'undefined') {
+      currentPage = +req.query.page;
+    }
+
+    filmsList = filmsArrays[+currentPage - 1];
 
     res.render('default', {
-    	films: filmsdata["films"]
+      films: filmsList,
+      pageCount: pageCount,
+      currentPage: currentPage
     });
 });
 
